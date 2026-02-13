966bba178da1ed8570d07c269bda7a6247079386
Juan Lago <juan@leadsupply.com>
2026-02-13 16:03:07 +0100

    Fixed test configuration.

--- test/TimeoutTestBase.php
+++ test/TimeoutTestBase.php
@@ -32,11 +32,11 @@ abstract class TimeoutTestBase extends TestCase
 
         config()->set('database.default', 'default');
         config()->set('database.connections.default', [
-            'driver' => env('DB_DRIVER', 'mysql'),
+            'driver' => env('DB_DRIVER', 'mariadb'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3308'),
+            'port' => env('DB_PORT', '3306'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'secret'),
+            'password' => env('DB_PASSWORD', ''),
             'database' => env('DB_DATABASE', ''),
             'prefix' => '',
         ]);
